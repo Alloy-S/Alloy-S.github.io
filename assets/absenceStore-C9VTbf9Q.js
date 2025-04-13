e065b0fe65177b5bd7d7c118c237c3bca19156ef
Alloy-S <107180700+Alloy-S@users.noreply.github.com>
2025-04-13 23:59:23 +0700

    update latest

--- assets/absenceStore-C9VTbf9Q.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./index-BCxpIQ8Y.js";const n=e("absence",{state:()=>({photo:null,attendanceData:null}),actions:{setPhoto(t){this.photo=t},setAttendance(t){this.attendanceData=t}}});export{n as u};
---
+++ assets/absenceStore-C9tPyxlM.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index-cI067N9l.js";const n=e("absence",{state:()=>({photo:null,attendanceData:null}),actions:{setPhoto(t){this.photo=t},setAttendance(t){this.attendanceData=t}}});export{n as u};
